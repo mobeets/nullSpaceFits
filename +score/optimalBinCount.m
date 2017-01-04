9223a8cde5079acb897f0e38d23790849058b556
mobeets <mobeets@gmail.com>
2017-01-04 15:10:17 -0500

    needed hist count for optimizing hist bin size

--- +score/optimalBinCount.m
+++ +score/optimalBinCount.m
@@ -1,7 +1,7 @@
-function h = optimalBinCount(Y, gs)
+function h = optimalBinCount(Y, gs, minBinSz)
 % for each group, for each column, calculate LOO c-v score for histogram
 %   for a range of bin counts
-% now, take the mean of the z-scores of these scores across columns
+% now, take the median of the z-scores of these scores across columns
 %   and then take the mean of these resulting scores across groups
 % returns the bin count minimizing the above score
 %
@@ -13,28 +13,36 @@ function h = optimalBinCount(Y, gs)
 %     Y = D.hyps(1).nullActivity.zNull;
 %     gs = D.blocks(2).thetaActualGrps;
 %     gs = ones(size(Y,1),1);
+    if nargin < 3
+        minBinSz = 10;
+    end
 
-    crit = @(cs,h,n) 2./((n-1).*h) - sum(cs.^2)*(n+1)./((n-1)*n^2.*h);
+%     crit = @(cs,h,n) 2./((n-1).*h) - sum(cs.^2)*(n+1)./((n-1)*n^2.*h);
     crit = @(cs,h,n) (2*mean(cs) - var(cs, 1))/h^2;
-    xs = 10:200;
+    hs = 1:200;
     
     grps = sort(unique(gs));
-    yc = cell(numel(grps),1);
+    sc = nan(numel(grps), numel(hs));
     for ii = 1:numel(grps)
         ix = grps(ii) == gs;
         Yc = Y(ix,:);
-        n = size(Yc,1);
-        rng = max(max(Yc)) - min(min(Yc)); % range of histogram
-
-        cfcn = @(h) score.marginalHist(Yc, ones(n,1), {}, h);
-        getcellind = @(fcn, ind) fcn{ind};
-        obj = @(h) crit(getcellind(cfcn(h), 1), rng/h, n); % rng/h == binsz
-        
-        ys = cell2mat(arrayfun(obj, xs, 'uni', 0)');
-        yc{ii} = mean(zscore(ys), 2);        
+        [nt, nd] = size(Yc);
+        mny = min(Yc(:)); mxy = max(Yc(:));
+        rng = mxy - mny;
+        scs = nan(numel(hs), nd);
+        for jj = 1:numel(hs)
+            xs = linspace(mny, mxy, hs(jj));
+            cs = hist(Yc, xs);
+            for kk = 1:nd
+                scs(jj,kk) = crit(cs(:,kk), rng/hs(jj), nt);
+            end
+        end
+        sc(ii,:) = median(zscore(scs),2);
     end
-    
-    ya = mean(cell2mat(yc'),2);
-    [~,ix] = min(ya);
-    h = xs(ix);
+    sc = median(sc);
+    [~,ix] = min(sc);
+    h = hs(ix);
+    if ~isnan(minBinSz)
+        h = max(h, minBinSz);
+    end
 end
